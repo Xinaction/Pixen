fdc5f4d543e881bfeaabd8ddb025ec45028c6abf
sana <takagisou@outlook.com>
2019-09-04 03:54:25 +0900

    Convert PercentFormatter to Swift

---
+++ Pixen Application/Application/PercentFormatter.swift
@@ -0,0 +1,31 @@
+//
+//  PercentFormatter.swift
+//  Pixen
+//
+//  Created by sana on 2019/09/04.
+//  Copyright © 2019 Pixen Project. All rights reserved.
+//
+
+import Foundation
+
+class PercentFormatter: NumberFormatter {
+    override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+
+        do {
+            let ex1 = try NSRegularExpression(pattern: "*^\\d+%$", options: [])
+            let ex2 = try NSRegularExpression(pattern: "*^\\d+$", options: [])
+            let match1 = ex1.numberOfMatches(in: partialString,
+                                             options: [],
+                                             range: NSMakeRange(0, partialString.count))
+            
+            let match2 = ex2.numberOfMatches(in: partialString,
+                                             options: [],
+                                             range: NSMakeRange(0, partialString.count))
+            
+            return (match1 + match2) > 0
+        } catch let err {
+            error?.pointee = err.localizedDescription as NSString
+            return false
+        }
+    }
+}
